82e69a21f4ad669319ad74adfedd3d4038d0da25
livedx <kolobokde@mail.ru>
2019-04-29 23:00:15 +0300

    merge

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,27 +5,13 @@ import createBrowserHistory from "history/createBrowserHistory";
 
 import './App.css';
 
-import PersonList from '../../components/PersonList'
-import PersonInput from '../../components/PersonInput'
-import Weather from '../../components/Weather'
-import Form from '../../components/Form'
 
 import Home from "../Home";
 import Registration from "../Registration";
 import Login from "../Login";
 const history = createBrowserHistory()
-const API_KEY= "c318a9a8eb7ccbb5ebca30e7240f5404";
 
 class App extends Component {
-
-    gettingWeather = async (e) =>{
-        e.preventDefault();
-        const city = e.target.elements.city.value;
-        const api_url = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-        const data = await api_url.json();
-        console.log(data);
-    }
-
     render() {
         return (
             <div>
@@ -37,12 +23,6 @@ class App extends Component {
                         <Route component={() => <span>404</span>}/>
                     </Switch>
                 </Router>
-
-                <PersonInput/>
-                <PersonList/>
-                <Form weatherMethod={this.gettingWeather} />
-                <Weather/>
-
             </div>
         );
     }
